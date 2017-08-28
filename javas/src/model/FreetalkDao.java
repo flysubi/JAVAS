b392f1719acf989dcea9dfa47d9b689a2989f2fa
1103-10 <1103-10@1103-10-PC>
2017-08-28 17:36:46 +0900

    오류수정

--- javas/src/model/FreetalkDao.java
+++ javas/src/model/FreetalkDao.java
@@ -13,9 +13,6 @@ import org.springframework.stereotype.Service;
 public class FreetalkDao {
 
 	@Autowired
-	DataSource ds;
-	
-	@Autowired
 	SqlSessionFactory factory;
 	
 	public List<Map> allTalks(Map map) {
