ebd41561bd1d3112b58399c576c9aec4d8988879
1103-10 <1103-10@1103-10-PC>
2017-08-28 16:11:13 +0900

    userController

--- javas/src/controller/UserController.java
+++ javas/src/controller/UserController.java
@@ -1,8 +1,17 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
-
+	
+	@RequestMapping("/join.sz")
+	public ModelAndView toJoin() {
+		ModelAndView mav = new ModelAndView("t_el");
+		mav.addObject("section", "user/join");
+		return mav;
+	}
 }
