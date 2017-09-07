33037bdc35a69e167598c1a81d10f63b4c4c2fd7
1103-12 <1103-12@1103-12-PC>
2017-09-07 19:31:47 +0900

    0907 commit2

--- javas/src/controller/CalendarController.java
+++ javas/src/controller/CalendarController.java
@@ -1,6 +1,6 @@
 package controller;
 
-import java.util.HashMap;
+import java.sql.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import model.CalendarDao;
@@ -35,9 +34,13 @@ public class CalendarController {
 	
 	@RequestMapping("/addCal.jv")
 	public ModelAndView addCal(@RequestParam Map map, HttpSession session) {
-		System.out.println(map);
 		String id = (String)session.getAttribute("auth");
 			map.put("id",id);
+			if(map.get("dday") != null) {
+				map.put("dday", "true");
+			} else {
+				map.put("dday", "false");
+			}
 		boolean b = cdao.addCal(map);
 		ModelAndView mav = new ModelAndView("t_el");
 			mav.addObject("rst", b);
@@ -47,16 +50,31 @@ public class CalendarController {
 	
 	@RequestMapping("/calModify.jv")
 	public ModelAndView calModify(@RequestParam Map map, @RequestParam (name="num") int n) {
-		System.out.println(map);
 		boolean flag = false;
 		if(map.get("mode").equals("delete")) {
 			flag = cdao.deleteCal(n);
 		} else {
+			if(map.get("dday") != null) {
+				map.put("dday", "true");
+			} else {
+				map.put("dday", "false");
+			}
+			System.out.println(map);
 			flag = cdao.updateCal(map);
 		}
-		ModelAndView mav = new ModelAndView("t_el");
+		ModelAndView mav = new ModelAndView();
 		
-			mav.addObject("section", "function/calendar");
+			mav.setViewName("redirect:/function/calendar.jv"); 
+		return mav;
+	}
+	
+	@RequestMapping("/calTest.jv")
+	public ModelAndView calTest(HttpSession session) {
+		String id = (String)session.getAttribute("auth");
+		ModelAndView mav = new ModelAndView("t_el");
+		List<Map> list = cdao.ddayCal(id);
+			mav.addObject("list",list);
+			mav.addObject("section", "function/calTest");
 		return mav;
 	}
 }
